0858f5e2f8a06adf6ab49d56e94b346afad135fc
Raphael Muniz <raphaelmunizvarela@gmail.com>
2026-05-30 23:29:37 -0300

    config-server

--- microsservicos-spring-rabbitmq-matchpass/event-catalog-service/src/main/java/event_catalog_service/controllers/TeamController.java
+++ microsservicos-spring-rabbitmq-matchpass/event-catalog-service/src/main/java/event_catalog_service/controllers/TeamController.java
@@ -3,16 +3,17 @@ package event_catalog_service.controllers;
 import event_catalog_service.dtos.req.TeamRequestDTO;
 import event_catalog_service.dtos.res.TeamResponseDTO;
 import event_catalog_service.services.TeamService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/team")
 public class TeamController {
     private final TeamService teamService;
+
+    @Value("${event-catalog-service.teste:valor-padrao}")
+    private String teste;
 
     public TeamController(TeamService teamService) {
         this.teamService = teamService;
@@ -21,5 +22,10 @@ public class TeamController {
     @PostMapping("/v1")
     public ResponseEntity<TeamResponseDTO> createTeam(@RequestBody TeamRequestDTO dto) {
         return ResponseEntity.ok().body(teamService.createTeam(dto));
+    }
+
+    @GetMapping("/test-config")
+    public ResponseEntity<String> getTestConfig() {
+        return ResponseEntity.ok("O valor da variável é: " + teste);
     }
 }
