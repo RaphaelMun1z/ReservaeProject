a8b690c330d1399450da27e9b3120ab94cbc3936
Raphael Muniz <raphaelmunizvarela@gmail.com>
2026-06-02 12:44:41 -0300

    feat: ticket service

---
+++ microsservicos-spring-rabbitmq-matchpass/ticket-service/src/main/java/ticket_service/StartupTicketService.java
@@ -0,0 +1,13 @@
+package ticket_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartupTicketService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartupTicketService.class, args);
+    }
+
+}
---
+++ microsservicos-spring-rabbitmq-matchpass/ticket-service/src/test/java/ticket_service/StartupTicketServiceTests.java
@@ -0,0 +1,13 @@
+package ticket_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTicketServiceTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
